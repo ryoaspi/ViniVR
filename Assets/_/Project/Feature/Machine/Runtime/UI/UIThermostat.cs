d1a01b1f4f3e3a7bb4a5ae0b0059fdf06646fbe1
Florentin D'Henau <florentin.dhenau@gmail.com>
2026-07-29 00:29:51 +0200

    [DEV] UI  go to temp random and UI disable on run

--- Assets/_/Project/Feature/Machine/Runtime/UI/UIThermostat.cs
+++ Assets/_/Project/Feature/Machine/Runtime/UI/UIThermostat.cs
@@ -19,6 +19,12 @@ public class UIThermostat : MonoBehaviour
         wishedTemperatureValue = uiDocumentReference.rootVisualElement.Q<Label>("wished-temperature-value");
         wishedTemperatureSlider.highValue = 32f;
         
+        tankStartButton = uiDocumentReference.rootVisualElement.Q<Button>("tank-start-button");
+        tankStartButton.clicked += StartThermoregulation;
+        
+        currentTemperatureValue = uiDocumentReference.rootVisualElement.Q<Label>("Temperature_value");
+        currentTemperatureValue.text = $"{currentTemperature:0.#}°C";
+        
         wishedTemperatureSlider.SetValueWithoutNotify(18f);
         wishedTemperatureValue.text = "18°C";
         
@@ -27,10 +33,27 @@ public class UIThermostat : MonoBehaviour
 
     void Update()
     {
+        if (!isThermoregulating)
+        {
+            return;
+        }
         
+        if (Time.time < nextTemperatureChangeTime)
+        {
+            return;
+        }
+        
+        currentTemperature = Mathf.MoveTowards(currentTemperature, targetTemperature, 0.5f);
+        currentTemperatureValue.text = $"{currentTemperature:0.#}°C";
+        nextTemperatureChangeTime = Time.time + Random.Range(2f, 5f);
+        
+        if (currentTemperature == targetTemperature)
+        {
+            isThermoregulating = false;
+            tankStartButton.SetEnabled(true);
+            wishedTemperatureSlider.SetEnabled(true);
+        }
     }
-
-
     #endregion
     
     #region Utils
@@ -41,11 +64,33 @@ public class UIThermostat : MonoBehaviour
         wishedTemperatureSlider.SetValueWithoutNotify(roundedValue);
         wishedTemperatureValue.text = $"{roundedValue:0.#}°C";
     }
+    
+    private void StartThermoregulation()
+    {
+        if (isThermoregulating)
+        {
+            return;
+        }
+        targetTemperature = wishedTemperatureSlider.value;
+        isThermoregulating = true;
+        tankStartButton.SetEnabled(false);
+        wishedTemperatureSlider.SetEnabled(false);
+        
+        //Randomize between 2 and 4 seconds each 0.5° step
+        nextTemperatureChangeTime = Time.time + Random.Range(2f, 5f);
+    }
     #endregion
     
     #region Private
     [SerializeField] private UIDocument uiDocumentReference;
+    [SerializeField] private float currentTemperature = 18.5f;
+    private float targetTemperature;
+    private bool isThermoregulating = false;
+    private float nextTemperatureChangeTime;
     private Slider wishedTemperatureSlider;
     private Label wishedTemperatureValue;
+    private Button tankStartButton;
+    private Label currentTemperatureValue;
+    
     #endregion
 }
