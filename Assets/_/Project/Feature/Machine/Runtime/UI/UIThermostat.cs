12ca6b86de297bcd31729d148c9f53a392b9def5
Florentin D'Henau <florentin.dhenau@gmail.com>
2026-07-28 17:36:58 +0200

    [DEV] UI Tank : added runtime heat value change

--- Assets/_/Project/Feature/Machine/Runtime/UI/UIThermostat.cs
+++ Assets/_/Project/Feature/Machine/Runtime/UI/UIThermostat.cs
@@ -1,34 +1,51 @@
 using UnityEngine;
 using UnityEngine.UIElements;
 
-public class ThermostatUI : MonoBehaviour
+public class UIThermostat : MonoBehaviour
 {
-    [SerializeField] private Label valueLabel;
-
-    private void OnEnable()
+    
+    #region Publics
+    #endregion
+    
+    #region Unity API
+    #endregion
+    
+    #region Main Methods
+    void Start()
     {
-        var uiDocument = GetComponent<UIDocument>();
-        var root = uiDocument.rootVisualElement;
-
-        Debug.Log("Root est null ? " + (root == null));
-        Debug.Log("Nombre d'enfants du root : " + (root != null ? root.childCount.ToString() : "N/A"));
-
-        var slider = root.Q<Slider>("wished-temperature-slider");
-        valueLabel = root.Q<Label>("temperature-value-label");
-
-        Debug.Log("Slider trouvé ? " + (slider != null));
-        Debug.Log("Label trouvé ? " + (valueLabel != null));
-
-        UpdateLabel(slider.value);
-
-        slider.RegisterValueChangedCallback(evt =>
-        {
-            UpdateLabel(evt.newValue);
-        });
+        wishedTemperatureSlider = uiDocumentReference.rootVisualElement.Q<Slider>("wished-temperature-slider");
+        wishedTemperatureSlider.lowValue = 10f;
+        
+        wishedTemperatureValue = uiDocumentReference.rootVisualElement.Q<Label>("wished-temperature-value");
+        wishedTemperatureSlider.highValue = 32f;
+        
+        wishedTemperatureSlider.SetValueWithoutNotify(18f);
+        wishedTemperatureValue.text = "18°C";
+        
+        wishedTemperatureSlider.RegisterValueChangedCallback(OnWishedTemperatureChanged);
     }
 
-    private void UpdateLabel(float value)
+    void Update()
     {
-        valueLabel.text = value.ToString("F1");
+        
     }
+
+
+    #endregion
+    
+    #region Utils
+    private void OnWishedTemperatureChanged(ChangeEvent<float> evt)
+    {
+        //Force a round or .5 value each slider step
+        float roundedValue = Mathf.Round(evt.newValue * 2f) / 2f;
+        wishedTemperatureSlider.SetValueWithoutNotify(roundedValue);
+        wishedTemperatureValue.text = $"{roundedValue:0.#}°C";
+    }
+    #endregion
+    
+    #region Private
+    [SerializeField] private UIDocument uiDocumentReference;
+    private Slider wishedTemperatureSlider;
+    private Label wishedTemperatureValue;
+    #endregion
 }
