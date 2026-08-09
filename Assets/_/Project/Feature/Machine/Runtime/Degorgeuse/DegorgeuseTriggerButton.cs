f2c5453df3400e4813bef9cadc9f639c8f71593a
Florentin D'Henau <florentin.dhenau@gmail.com>
2026-08-09 23:56:15 +0200

    [DEV] Added sound effects on Degorgeuse animation

--- Assets/_/Project/Feature/Machine/Runtime/Degorgeuse/DegorgeuseTriggerButton.cs
+++ Assets/_/Project/Feature/Machine/Runtime/Degorgeuse/DegorgeuseTriggerButton.cs
@@ -7,9 +7,12 @@ namespace Machine.Runtime
         public void TriggerFilling()
         {
             _animator.SetTrigger("isFilling");
+            _pistonAudioSource.Play();
         }
         
 
         [SerializeField] private Animator _animator;
+        [SerializeField] private AudioSource _pistonAudioSource;
+
     }
 }
