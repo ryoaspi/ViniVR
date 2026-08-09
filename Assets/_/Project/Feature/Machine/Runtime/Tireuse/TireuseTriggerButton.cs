8fd3bd47fc27022ff823a77e12e380830e7f4e2e
Florentin D'Henau <florentin.dhenau@gmail.com>
2026-08-09 23:45:14 +0200

    [DEV] Added sound effects on all tireuse animations

--- Assets/_/Project/Feature/Machine/Runtime/Tireuse/TireuseTriggerButton.cs
+++ Assets/_/Project/Feature/Machine/Runtime/Tireuse/TireuseTriggerButton.cs
@@ -7,18 +7,36 @@ namespace Machine.Runtime
         public void TriggerFilling()
         {
             _animator.SetTrigger("isFilling");
+            _pistonsAudioSource.Play();
         }
 
-        public void TireuseDoorOpening()
+        public void ToggleDoor()
         {
-            _animator.SetTrigger("isDoorOpening");
-        }
+            if (_isDoorOpen)
+            {
+                _animator.SetTrigger("isDoorClosing");
+                _doorAudioSource.PlayOneShot(_doorClosingClip);
+                _isDoorOpen = false;
+            }
+            else
+            {
+                _animator.SetTrigger("isDoorOpening");
+                _doorAudioSource.PlayOneShot(_doorOpeningClip);
+                _isDoorOpen = true;
+            }
+        } 
 
         public void TireuseCapsuling()
         {
             _animator.SetTrigger("isCapsuling");
+            _pistonsAudioSource.Play();
         }
 
         [SerializeField] private Animator _animator;
+        [SerializeField] private AudioSource _pistonsAudioSource;
+        [SerializeField] private AudioSource _doorAudioSource;
+        [SerializeField] private AudioClip _doorOpeningClip;
+        [SerializeField] private AudioClip _doorClosingClip;
+        private bool _isDoorOpen = false;
     }
 }
