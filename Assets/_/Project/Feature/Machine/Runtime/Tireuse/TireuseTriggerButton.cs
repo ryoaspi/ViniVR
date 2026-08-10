f166b25f2887e58e7c083f90079486a1445f4718
ryoaspi <>
2026-08-10 16:54:48 +0200

    [Dev] save for day

--- Assets/_/Project/Feature/Machine/Runtime/Tireuse/TireuseTriggerButton.cs
+++ Assets/_/Project/Feature/Machine/Runtime/Tireuse/TireuseTriggerButton.cs
@@ -25,11 +25,65 @@ namespace Machine.Runtime
                 _isDoorOpen = true;
             }
         } 
+        
+        public void FillAllBottles()
+        {
+            for (int i = 0; i < _bottles.Length; i++)
+            {
+                if (_bottles[i] == null)
+                    continue;
+
+                _bottles[i].FillBottle();
+            }
+        }
+        
+        public void SetBottle(int index, GameObject bottle)
+        {
+            if (!IsValidBottleIndex(index))
+                return;
+
+            if (bottle == null)
+            {
+                _bottles[index] = null;
+                return;
+            }
+
+            _bottles[index] =
+                bottle.GetComponentInChildren<BottleLiquidFill>();
+
+            if (_bottles[index] == null)
+            {
+                Debug.LogWarning(
+                    $"[TireuseTriggerButton] Aucun BottleLiquidFill trouvé sur {bottle.name}.",
+                    bottle);
+            }
+        }
+        
+        public void ClearBottle(int index)
+        {
+            if (!IsValidBottleIndex(index))
+                return;
+
+            _bottles[index] = null;
+        }
+        
 
         public void TireuseCapsuling()
         {
             _animator.SetTrigger("isCapsuling");
             _pistonsAudioSource.Play();
+        }
+        
+        private bool IsValidBottleIndex(int index)
+        {
+            if (index >= 0 && index < _bottles.Length)
+                return true;
+
+            Debug.LogWarning(
+                $"[TireuseTriggerButton] Index bouteille invalide : {index}.",
+                this);
+
+            return false;
         }
 
         [SerializeField] private Animator _animator;
@@ -37,6 +91,7 @@ namespace Machine.Runtime
         [SerializeField] private AudioSource _doorAudioSource;
         [SerializeField] private AudioClip _doorOpeningClip;
         [SerializeField] private AudioClip _doorClosingClip;
+        private BottleLiquidFill[] _bottles = new BottleLiquidFill[3];
         private bool _isDoorOpen = false;
     }
 }
